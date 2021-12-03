4554b1e305979b904bf8704f8fcce39bbcabcc61
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2021-12-03 16:20:13 -0500

    Completed Lecture 1
    This is the complete Tomb of Horrors lecture 1. All tests and functionality is present.

--- tombLecture/tombProject/urls.py
+++ tombLecture/tombProject/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from tombApp import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
+    url(r'^lore.html$', views.lore_page, name='lore'),
+    url(r'^home.html$', views.home_page, name='home'),
 ]
